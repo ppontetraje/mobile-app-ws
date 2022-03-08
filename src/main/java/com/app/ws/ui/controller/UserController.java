253e6ec1547ff29172f58267a6fee89795386310
JPiert <jpalominoq@unsa.edu.pe>
2022-03-08 18:02:27 -0500

    Se testeó cada una de los métodos de petició, se creó una clase de usuario, se testeó el retorno de un usuario por el método get

--- src/main/java/com/app/ws/ui/controller/UserController.java
+++ src/main/java/com/app/ws/ui/controller/UserController.java
@@ -2,18 +2,36 @@ package com.app.ws.ui.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.ws.ui.model.response.UserRest;
+
 @RestController
-@RequestMapping("users") //http://localhost:8080/users
+@RequestMapping("/users") //http://localhost:8080/users
 public class UserController {
 	
 	@GetMapping
-	public String getUser() {
-		return "get user was called";
+	public String getUsers(@RequestParam(value="page", defaultValue="1") int page,
+			@RequestParam(value="limit", defaultValue="1") int limit,
+			@RequestParam(value="sort", defaultValue="desc", required=false) String sort){
+		
+			
+		return "get user was called page= "+page+" limit= "+limit+" sort = "+sort;
+	
+	}
+	
+	@GetMapping(path="/{userId}")
+	public UserRest getUser(@PathVariable String userId) {
+		UserRest returnValue = new UserRest();
+		returnValue.setEmail("test@email.com");
+		returnValue.setFirstName("Jean");
+		returnValue.setLastName("palomino");
+		return returnValue;
 	
 	}
 	@PostMapping
