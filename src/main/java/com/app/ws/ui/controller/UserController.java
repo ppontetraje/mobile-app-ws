a115e52600027391d4a146f0bc83c007ba813e54
JPiert <jpalominoq@unsa.edu.pe>
2022-03-10 18:02:27 -0500

    se utiliza la dependencia devtools para recarga de automática de cambios, Se probó el método get y Post de la clase usuario, se utiliza también la dependencia de validaciones para evitar errores en el envío de datos.

--- src/main/java/com/app/ws/ui/controller/UserController.java
+++ src/main/java/com/app/ws/ui/controller/UserController.java
@@ -1,19 +1,34 @@
 package com.app.ws.ui.controller;
 
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.app.ws.ui.model.response.UserRest;
 
 @RestController
 @RequestMapping("/users") //http://localhost:8080/users
 public class UserController {
+	
+	Map<String, UserRest> users;
 	
 	@GetMapping
 	public String getUsers(@RequestParam(value="page", defaultValue="1") int page,
@@ -25,18 +40,38 @@ public class UserController {
 	
 	}
 	
-	@GetMapping(path="/{userId}")
-	public UserRest getUser(@PathVariable String userId) {
-		UserRest returnValue = new UserRest();
-		returnValue.setEmail("test@email.com");
-		returnValue.setFirstName("Jean");
-		returnValue.setLastName("palomino");
-		return returnValue;
-	
+	@GetMapping(path="/{userId}", 
+			produces = {
+					MediaType.APPLICATION_XML_VALUE, 
+					MediaType.APPLICATION_JSON_VALUE
+					})
+	public ResponseEntity<UserRest>  getUser(@PathVariable String userId) {
+		if (users.containsKey(userId)) {
+			return new ResponseEntity<>(users.get(userId),HttpStatus.OK );
+		}else {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}	
 	}
-	@PostMapping
-	public String createUser() {
-		return "create user was called";
+	@PostMapping(consumes = {
+			MediaType.APPLICATION_XML_VALUE, 
+			MediaType.APPLICATION_JSON_VALUE
+	}, produces = {
+			MediaType.APPLICATION_XML_VALUE, 
+			MediaType.APPLICATION_JSON_VALUE
+			}
+			)
+	public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailsRequestModel userDetails) {
+		UserRest returnValue = new UserRest();
+		returnValue.setEmail(userDetails.getEmail());
+		returnValue.setFirstName(userDetails.getFirstName());
+		returnValue.setLastName(userDetails.getLastName());
+		String userId = UUID.randomUUID().toString();
+		returnValue.setUserId(userId);
+		
+		if (users == null) users = new HashMap<>();
+		users.put(userId, returnValue);
+		
+		return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
 	}
 	@PutMapping
 	public String updateUser() {
